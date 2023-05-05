7f222c31183ab30469041834b676ad1a80797dd4
Blackstar84 <carlosalmada84@gmail.com>
2023-05-05 13:51:55 -0400

    se crean copias de post.js y column.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,106 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { SketchPicker } from 'react-color';
 import axios from 'axios';
 import Post from './components/Post';
-//import Column  from './components/Column';
-
-const Column = (props) => {
-  const [title, setTitle] = useState('');
-  const category = props.cat;
-  const [color, setColor] = useState('#ffffff');
-  const [items, setItems] = useState([]);
-  const [update, setUpdate] = useState(false);
-
-  const handleInputChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleColorChange = (color) => {
-    setColor(color.hex);
-  };
-  
-  
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      console.log(title, category, color);
-      setItems([...items, { value: title, color }]);
-      
-      axios.post('http://localhost:5000/createPost', {
-        title, category, color
-    }).then(() => alert('Datos guardados!')).catch((error) => console.log(error));
-    setTitle('');
-    setUpdate(true);  
-    }
-  };
-
-  /* const [wentWell, setWell] = useState([]);
-  const [toImprove, setImprove] = useState([]);
-  const [kudos, setKudos] = useState([]);
-  const [update, setUpdate] = useState(false);
-let categoria = '';
+import Column  from './components/Column';
 
 
-useEffect(() => {
-  const getTodos = (categoria) => {
-    axios.get(`http://localhost:5000/posts?category=${categoria}`)
-      .then(res => {
-        console.log(res);
-        switch (categoria) {
-          case 'wentWell':
-            setWell(res.data)    
-            break;
-          case 'toImprove':
-            setImprove(res.data)
-            break;
-          case 'kudos':
-            setKudos(res.data)
-            break;   
-        }
-        
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-  getTodos('wentWell');
-  getTodos('toImprove');
-  getTodos('kudos');
-  setUpdate(false);
-},[update])  */
-  
 
-  const handleEdit = (index, newValue) => {
-    const newItems = [...items];
-    newItems[index].value = newValue;
-    setItems(newItems);
-  };
-
-  return (
-    <div className='col'>
-      <input
-        type="text"
-        value={title}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      />
-      <SketchPicker color={color} onChange={handleColorChange} />
-
-     {/*  { 
-      wentWell.length > 0 && wentWell.map((item, index) => (
-        <div key={index} style={{ backgroundColor: item.color }}>
-          <input
-            type="text"
-            value={item.title}
-            onChange={(e) => handleEdit(e.key,index, e.target.value)}
-          />
-        </div>
-        
-      ))} */}
-    </div>
-  );
-};
 
 const App = () => (
+  
     <div className='container'>
       <div className="row align-items-start">
         <Column cat='wentWell' />
@@ -109,9 +16,9 @@ const App = () => (
         <Column cat='kudos' />
       </div>
       <div className="row align-items-start">
-        <Post categoria='wentWell' />
-        <Post categoria='toImprove' />
-        <Post categoria='kudos' />
+        <Post categoria='wentWell' update={true} />
+        <Post categoria='toImprove' update={true} />
+        <Post categoria='kudos' update={true} />
       </div>
     </div>
 );
